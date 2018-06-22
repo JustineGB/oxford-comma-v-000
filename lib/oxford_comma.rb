9fba9073016976942c0a3d5b01987215e6ab6f93
Justine Bricknell <justinegayton@gmail.com>
2018-06-22 15:26:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,5 @@
 def oxford_comma(array)
- if array.length == 1 return array[0]
+  if array.length == 1 return array[0]
+  if array.lenght == 2 return array.join(",")
 #return array[0..-2].join(",") + "and" + arrary[-1] if array.length > 1
 end
