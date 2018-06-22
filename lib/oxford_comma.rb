80d2e13ff57a4e3e7d5ef2bf8f8656c1da4a3c68
Justine Bricknell <justinegayton@gmail.com>
2018-06-22 14:28:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
-
+#add "and" when just two words
+#add commas btw words and add a comma&and for last word in list
+#take in argument of array and convert this to a string following oxford comma rules. could use .join
+array.join(",")
 end
