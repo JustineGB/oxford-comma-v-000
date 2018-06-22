56091fa1bf4956ebb27cf43c38f820d45f01286e
Justine Bricknell <justinegayton@gmail.com>
2018-06-22 15:07:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,15 @@
 def oxford_comma(array)
-  if array.length == 2 
-    array.join(",")
-  elsif array.length > 3
-    array.join("," last.element ", and")
-  end
+  array.to_sentence(two_words_connector: ",")
+  #if array.size < 2
+  #  array.join("")
+#  elsif array.size == 2
+#  else array.size > 3
+#    array.join("," last ", and")
+    
+#    array.join(",")
+#  elsif array.length > 3
+#    array.join("," last.element ", and")
+#  end
 end
 #if array.length == 2
 #  array.join(",")
