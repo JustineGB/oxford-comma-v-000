e5ef065272eac1ca9648ecf22b294d4f172f5cec
Justine Bricknell <justinegayton@gmail.com>
2018-06-22 15:27:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
-    return array[0]
-  if array.lenght == 2
+    return array.join("")
+  elsif array.lenght == 2
     return array.join(",")
 #return array[0..-2].join(",") + "and" + arrary[-1] if array.length > 1
 end
